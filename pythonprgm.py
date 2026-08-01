31b0020443c1497018894120da7414fb8b21df6b
maha0330 <maha03302007@gmail.com>
2026-08-01 16:22:36 +0530

    array operation

---
+++ pythonprgm.py
@@ -0,0 +1,10 @@
+#numpy array operations
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("array:",arr)
+print("sum:",np.sum(arr))
+print("mean:",np.mean(arr))
+print("maximum:",np.max(arr))
+print("minimum:",np.min(arr))
+new_arr=arr*2
+print("array after multiplying by 2:",new_arr)
