257680ff3dd17dd85c8c2454b89658d075b3b119
maha0330 <maha03302007@gmail.com>
2026-08-08 00:01:38 +0530

    shorthand op

---
+++ shorthand op.py
@@ -0,0 +1,24 @@
+# Shorthand Operators in Python
+
+a = 10
+
+a += 5
+print("After += :", a)
+
+a -= 3
+print("After -= :", a)
+
+a *= 2
+print("After *= :", a)
+
+a /= 4
+print("After /= :", a)
+
+a //= 2
+print("After //=:", a)
+
+a %= 3
+print("After %= :", a)
+
+a **= 2
+print("After **=:", a)
